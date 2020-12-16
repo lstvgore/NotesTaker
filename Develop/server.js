5717cfa2a1e32d3a4931ceaba066855181b5caec
stvgo15 <stv.gore@gmail.com>
2020-12-16 13:13:07 -0600

    notes are working

--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,11 @@ const { dirname } = require("path");
 const path = require("path");
 const fs = require("fs");
 
+let note =[{ id: 1, body: 'text here'},{id: 2, body: 'this is a second text'}]
+
 
 const app = express();
+const newLocal = 3000;
 const PORT = process.env.PORT||3000;
 
 
@@ -31,6 +34,40 @@ app.get('/api/notes', function(req, res){
    })
 })
 
+app.post('/api/notes', function(req, res){
+  let SaveNotes = [];
+  const NewNotes = req.body;
+  console.log(req)
+  fs.readFile(path.join(__dirname,"db","db.json"),"utf8", function(error,data){
+    if(error){return console.log(error)}
+    else{
+        SaveNotes = JSON.parse(data)
+        NewNotes.id = parseInt(Math.random()*10000)
+        SaveNotes.push(NewNotes)
+        console.log(SaveNotes)
+        fs.writeFile(path.join(__dirname,"db","db.json"), JSON.stringify(SaveNotes), function(error){
+            if(error){return res.status(500)}
+            else{
+                res.status(200)
+            }
+        })
+    }
+})
+  console.log(SaveNotes)
+  console.log(req.body)
+  res.redirect('/');
+});
+
 app.listen(PORT, function(){
     console.log("server start running")
 });
+
+
+
+
+
+
+
+
+
+
